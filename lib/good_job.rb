2e981c691e1c8e2728113f98a42f915330695a01
Ben Sheldon <bensheldon@gmail.com>
2020-08-19 18:36:48 -0700

    Document GoodJob module

--- lib/good_job.rb
+++ lib/good_job.rb
@@ -15,13 +15,38 @@ require 'good_job/notifier'
 
 require 'active_job/queue_adapters/good_job_adapter'
 
+# GoodJob is a multithreaded, Postgres-based, ActiveJob backend for Ruby on Rails.
+#
+# +GoodJob+ is the top-level namespace and exposes configuration attributes.
 module GoodJob
+  # @!attribute [rw] logger
+  #   @!scope class
+  #   The logger used by GoodJob
+  #   @return [Logger]
   mattr_accessor :logger, default: ActiveSupport::TaggedLogging.new(ActiveSupport::Logger.new(STDOUT))
-  mattr_accessor :preserve_job_records, default: false
-  mattr_accessor :reperform_jobs_on_standard_error, default: true
-  mattr_accessor :on_thread_error, default: nil
 
-  ActiveSupport.run_load_hooks(:good_job, self)
+  # @!attribute [rw] preserve_job_records
+  #   @!scope class
+  #   Whether to preserve job records in the database after they have finished for inspection
+  #   @return [Boolean]
+  mattr_accessor :preserve_job_records, default: false
+
+  # @!attribute [rw] reperform_jobs_on_standard_error
+  #   @!scope class
+  #   Whether to re-perform a job when a type of +StandardError+ is raised and bubbles up to the GoodJob backend
+  #   @return [Boolean]
+  mattr_accessor :reperform_jobs_on_standard_error, default: true
+
+  # @!attribute [rw] on_thread_error
+  #   @!scope class
+  #   Called when a thread raises an error
+  #   @example Send errors to Sentry
+  #     # config/initializers/good_job.rb
+  #
+  #     # With Sentry (or Bugsnag, Airbrake, Honeybadger, etc.)
+  #     GoodJob.on_thread_error = -> (exception) { Raven.capture_exception(exception) }
+  #   @return [#call, nil]
+  mattr_accessor :on_thread_error, default: nil
 
   # Shuts down all execution pools
   # @param wait [Boolean] whether to wait for shutdown
@@ -43,4 +68,6 @@ module GoodJob
     Notifier.instances.each(&:restart)
     Scheduler.instances.each(&:restart)
   end
+
+  ActiveSupport.run_load_hooks(:good_job, self)
 end
