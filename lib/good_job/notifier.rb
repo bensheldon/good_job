4371f1a637d20b24ca3419f63e01ff9b0934673b
Samuel Cochran <sj26@sj26.com>
2020-08-26 00:31:42 +1000

    Own ActiveRecord connection
    Take a leaf from actioncable's book and explicitly checkout and own a
    connection so we don't have to worry about locking.
    
    See: https://github.com/rails/rails/blob/acaa546026471354ae1ff15e96c6bf5d39b0e34a/actioncable/lib/action_cable/subscription_adapter/postgresql.rb#L36-L49

--- lib/good_job/notifier.rb
+++ lib/good_job/notifier.rb
@@ -70,36 +70,37 @@ module GoodJob # :nodoc:
     def listen
       future = Concurrent::Future.new(args: [@recipients, @pool, @listening], executor: @pool) do |recipients, pool, listening|
         Rails.application.reloader.wrap do
-          conn = ActiveRecord::Base.connection.raw_connection
-          ActiveSupport::Notifications.instrument("notifier_listen.good_job") do
-            conn.async_exec "LISTEN #{CHANNEL}"
-          end
-
-          begin
-            ActiveSupport::Dependencies.interlock.permit_concurrent_loads do
-              while pool.running?
-                listening.make_true
-                conn.wait_for_notify(WAIT_INTERVAL) do |channel, _pid, payload|
-                  listening.make_false
-                  next unless channel == CHANNEL
-
-                  ActiveSupport::Notifications.instrument("notifier_notified.good_job", { payload: payload })
-                  parsed_payload = JSON.parse(payload, symbolize_names: true)
-                  recipients.each do |recipient|
-                    target, method_name = recipient.is_a?(Array) ? recipient : [recipient, :call]
-                    target.send(method_name, parsed_payload)
-                  end
-                end
-                listening.make_false
-              end
+          with_listen_connection do |conn|
+            ActiveSupport::Notifications.instrument("notifier_listen.good_job") do
+              conn.async_exec "LISTEN #{CHANNEL}"
             end
-          rescue StandardError => e
-            ActiveSupport::Notifications.instrument("notifier_notify_error.good_job", { error: e })
-            raise
-          ensure
-            @listening.make_false
-            ActiveSupport::Notifications.instrument("notifier_unlisten.good_job") do
-              conn.async_exec "UNLISTEN *"
+
+            begin
+              ActiveSupport::Dependencies.interlock.permit_concurrent_loads do
+                while pool.running?
+                  listening.make_true
+                  conn.wait_for_notify(WAIT_INTERVAL) do |channel, _pid, payload|
+                    listening.make_false
+                    next unless channel == CHANNEL
+
+                    ActiveSupport::Notifications.instrument("notifier_notified.good_job", { payload: payload })
+                    parsed_payload = JSON.parse(payload, symbolize_names: true)
+                    recipients.each do |recipient|
+                      target, method_name = recipient.is_a?(Array) ? recipient : [recipient, :call]
+                      target.send(method_name, parsed_payload)
+                    end
+                  end
+                  listening.make_false
+                end
+              end
+            rescue StandardError => e
+              ActiveSupport::Notifications.instrument("notifier_notify_error.good_job", { error: e })
+              raise
+            ensure
+              @listening.make_false
+              ActiveSupport::Notifications.instrument("notifier_unlisten.good_job") do
+                conn.async_exec "UNLISTEN *"
+              end
             end
           end
         end
@@ -111,6 +112,17 @@ module GoodJob # :nodoc:
 
     def listen_observer(_time, _result, _thread_error)
       listen unless shutdown?
+    end
+
+    def with_listen_connection
+      ar_conn = ActiveRecord::Base.connection_pool.checkout.tap do |conn|
+        ActiveRecord::Base.connection_pool.remove(conn)
+      end
+      pg_conn = ar_conn.raw_connection
+      pg_conn.exec("SET application_name = #{pg_conn.escape_identifier(self.class.name)}")
+      yield pg_conn
+    ensure
+      ar_conn.disconnect!
     end
   end
 end
