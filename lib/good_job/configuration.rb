2f1786b3e131ef8ce965f2b9d1e31d52d8c889d1
Rob Brackett <rob@robbrackett.com>
2020-09-01 00:04:11 -0700

    Document the rest of the code
    This brings GoodJob to 89% documented -- the remaining bits are the methods on `LogSubscriber`, which didn't seem worth documenting since they merely mirror the names of notifications they respond to, so I feel like they're pretty self documenting. These can probably be handled with one of:
    - `:nodoc:` comments (which unfortunately show up in the resulting docs)
    - A macro that makes a pretty simplistic comment
    - `@api nodoc` comments combined with `--hide-api nodoc` in the `.yardopts` file. This will hide them completely.
    
    This also adds the `yard-activesupport-concern` gem, which is pretty helpful for documenting methods inside `included` and `class_methods` blocks, which are Rails features that are wind up hiding their contents from Yard.

--- lib/good_job/configuration.rb
+++ lib/good_job/configuration.rb
@@ -1,12 +1,40 @@
 module GoodJob
+  #
+  # +GoodJob::Configuration+ provides normalized configuration information to
+  # the rest of GoodJob. It combines environment information with explicitly
+  # set options to get the final values for each option.
+  #
   class Configuration
+    # @!attribute [r] options
+    #   The options that were explicitly set when initializing +Configuration+.
+    #   @return [Hash]
+    #
+    # @!attribute [r] env
+    #   The environment from which to read GoodJob's environment variables. By
+    #   default, this is the current process's environment, but it can be set
+    #   to something else in {#initialize}.
+    #   @return [Hash]
     attr_reader :options, :env
 
+    # @param options [Hash] Any explicitly specified configuration options to
+    #   use. Keys are symbols that match the various methods on this class.
+    # @param env [Hash] A +Hash+ from which to read environment variables that
+    #   might specify additional configuration values.
     def initialize(options, env: ENV)
       @options = options
       @env = env
     end
 
+    # Specifies how and where jobs should be executed. See {Adapter#initialize}
+    # for more details on possible values.
+    #
+    # When running inside a Rails app, you may want to use
+    # {#rails_execution_mode}, which takes the current Rails environment into
+    # account when determining the final value.
+    #
+    # @param default [Symbol]
+    #   Value to use if none was specified in the configuration.
+    # @return [Symbol]
     def execution_mode(default: :external)
       if options[:execution_mode]
         options[:execution_mode]
@@ -17,6 +45,9 @@ module GoodJob
       end
     end
 
+    # Like {#execution_mode}, but takes the current Rails environment into
+    # account (e.g. in the +test+ environment, it falls back to +:inline+).
+    # @return [Symbol]
     def rails_execution_mode
       if execution_mode(default: nil)
         execution_mode
@@ -29,6 +60,10 @@ module GoodJob
       end
     end
 
+    # Indicates the number of threads to use per {Scheduler}. Note that
+    # {#queue_string} may provide more specific thread counts to use with
+    # individual schedulers.
+    # @return [Integer]
     def max_threads
       (
         options[:max_threads] ||
@@ -38,12 +73,21 @@ module GoodJob
       ).to_i
     end
 
+    # Describes which queues to execute jobs from and how those queues should
+    # be grouped into {Scheduler} instances. See
+    # {file:README.md#optimize-queues-threads-and-processes} for more details
+    # on the format of this string.
+    # @return [String]
     def queue_string
       options[:queues] ||
         env['GOOD_JOB_QUEUES'] ||
         '*'
     end
 
+    # The number of seconds between polls for jobs. GoodJob will execute jobs
+    # on queues continuously until a queue is empty, at which point it will
+    # poll (using this interval) for new queued jobs to execute.
+    # @return [Integer]
     def poll_interval
       (
         options[:poll_interval] ||
--- lib/good_job/lockable.rb
+++ lib/good_job/lockable.rb
@@ -1,10 +1,33 @@
 module GoodJob
+  #
+  # Adds Postgres advisory locking capabilities to an ActiveRecord record.
+  # For details on advisory locks, see the Postgres documentation:
+  # - {https://www.postgresql.org/docs/current/explicit-locking.html#ADVISORY-LOCKS Advisory Locks Overview}
+  # - {https://www.postgresql.org/docs/current/functions-admin.html#FUNCTIONS-ADVISORY-LOCKS Advisory Locks Functions}
+  #
+  # @example Add this concern to a +MyRecord+ class:
+  #   class MyRecord < ActiveRecord::Base
+  #     include Lockable
+  #
+  #     def my_method
+  #       ...
+  #     end
+  #   end
+  #
   module Lockable
     extend ActiveSupport::Concern
 
+    # Indicates an advisory lock is already held on a record by another
+    # database session.
     RecordAlreadyAdvisoryLockedError = Class.new(StandardError)
 
     included do
+      # Attempt to acquire an advisory lock on the selected records and
+      # return only those records for which a lock could be acquired.
+      # @!method advisory_lock
+      # @!scope class
+      # @return [ActiveRecord::Relation]
+      #   A relation selecting only the records that were locked.
       scope :advisory_lock, (lambda do
         original_query = self
 
@@ -21,6 +44,17 @@ module GoodJob
         unscoped.where(arel_table[primary_key].in(query)).merge(original_query.only(:order))
       end)
 
+      # Joins the current query with Postgres's +pg_locks+ table (it provides
+      # data about existing locks) such that each row in the main query joins
+      # to all the advisory locks associated with that row.
+      #
+      # For details on +pg_locks+, see
+      # {https://www.postgresql.org/docs/current/view-pg-locks.html}.
+      # @!method joins_advisory_locks
+      # @!scope class
+      # @return [ActiveRecord::Relation]
+      # @example Get the records that have a session awaiting a lock:
+      #   MyLockableRecord.joins_advisory_locks.where("pg_locks.granted = ?", false)
       scope :joins_advisory_locks, (lambda do
         join_sql = <<~SQL
           LEFT JOIN pg_locks ON pg_locks.locktype = 'advisory'
@@ -32,16 +66,55 @@ module GoodJob
         joins(sanitize_sql_for_conditions([join_sql, { table_name: table_name }]))
       end)
 
+      # Find records that do not have an advisory lock on them.
+      # @!method advisory_unlocked
+      # @!scope class
+      # @return [ActiveRecord::Relation]
       scope :advisory_unlocked, -> { joins_advisory_locks.where(pg_locks: { locktype: nil }) }
+
+      # Find records that have an advisory lock on them.
+      # @!method advisory_locked
+      # @!scope class
+      # @return [ActiveRecord::Relation]
       scope :advisory_locked, -> { joins_advisory_locks.where.not(pg_locks: { locktype: nil }) }
+
+      # Find records with advisory locks owned by the current Postgres
+      # session/connection.
+      # @!method advisory_locked
+      # @!scope class
+      # @return [ActiveRecord::Relation]
       scope :owns_advisory_locked, -> { joins_advisory_locks.where('"pg_locks"."pid" = pg_backend_pid()') }
 
+      # @!attribute [r] create_with_advisory_lock
+      # @return [Boolean]
+      # Whether an advisory lock should be acquired immediately after creating
+      # the record.
+      # @example
+      #   record = MyLockableRecord.create(create_with_advisory_lock: true)
+      #   record.advisory_locked?
+      #   => true
       attr_accessor :create_with_advisory_lock
 
       after_create -> { advisory_lock }, if: :create_with_advisory_lock
     end
 
     class_methods do
+      # Acquires an advisory lock on the selected record(s) and safely releases
+      # it after the passed block is completed. The block will be passed an
+      # array of the locked records as its first argument.
+      #
+      # Note that this will not block and wait for locks to be acquired.
+      # Instead, it will acquire a lock on all the selected records that it
+      # can (as in {Lockable.advisory_lock}) and only pass those that could be
+      # locked to the block.
+      #
+      # @yield [Array<Lockable>] the records that were successfully locked.
+      # @return [Object] the result of the block.
+      #
+      # @example Work on the first two +MyLockableRecord+ objects that could be locked:
+      #   MyLockableRecord.order(created_at: :asc).limit(2).with_advisory_lock do |record|
+      #     do_something_with record
+      #   end
       def with_advisory_lock
         raise ArgumentError, "Must provide a block" unless block_given?
 
@@ -54,6 +127,11 @@ module GoodJob
       end
     end
 
+    # Acquires an advisory lock on this record if it is not already locked by
+    # another database session. Be careful to ensure you release the lock when
+    # you are done with {#advisory_unlock} (or {#advisory_unlock!} to release
+    # all remaining locks).
+    # @return [Boolean] whether the lock was acquired.
     def advisory_lock
       where_sql = <<~SQL
         pg_try_advisory_lock(('x' || substr(md5(:table_name || :id::text), 1, 16))::bit(64)::bigint)
@@ -61,6 +139,10 @@ module GoodJob
       self.class.unscoped.where(where_sql, { table_name: self.class.table_name, id: send(self.class.primary_key) }).exists?
     end
 
+    # Releases an advisory lock on this record if it is locked by this database
+    # session. Note that advisory locks stack, so you must call
+    # {#advisory_unlock} and {#advisory_lock} the same number of times.
+    # @return [Boolean] whether the lock was released.
     def advisory_unlock
       where_sql = <<~SQL
         pg_advisory_unlock(('x' || substr(md5(:table_name || :id::text), 1, 16))::bit(64)::bigint)
@@ -68,11 +150,28 @@ module GoodJob
       self.class.unscoped.where(where_sql, { table_name: self.class.table_name, id: send(self.class.primary_key) }).exists?
     end
 
+    # Acquires an advisory lock on this record or raises
+    # {RecordAlreadyAdvisoryLockedError} if it is already locked by another
+    # database session.
+    # @raise [RecordAlreadyAdvisoryLockedError]
+    # @return [Boolean] +true+
     def advisory_lock!
       result = advisory_lock
       result || raise(RecordAlreadyAdvisoryLockedError)
     end
 
+    # Acquires an advisory lock on this record and safely releases it after the
+    # passed block is completed. If the record is locked by another database
+    # session, this raises {RecordAlreadyAdvisoryLockedError}.
+    #
+    # @yield Nothing
+    # @return [Object] The result of the block.
+    #
+    # @example
+    #   record = MyLockableRecord.first
+    #   record.with_advisory_lock do
+    #     do_something_with record
+    #   end
     def with_advisory_lock
       raise ArgumentError, "Must provide a block" unless block_given?
 
@@ -82,14 +181,21 @@ module GoodJob
       advisory_unlock unless $ERROR_INFO.is_a? RecordAlreadyAdvisoryLockedError
     end
 
+    # Tests whether this record has an advisory lock on it.
+    # @return [Boolean]
     def advisory_locked?
       self.class.unscoped.advisory_locked.where(id: send(self.class.primary_key)).exists?
     end
 
+    # Tests whether this record is locked by the current database session.
+    # @return [Boolean]
     def owns_advisory_lock?
       self.class.unscoped.owns_advisory_locked.where(id: send(self.class.primary_key)).exists?
     end
 
+    # Releases all advisory locks on the record that are held by the current
+    # database session.
+    # @return [nil]
     def advisory_unlock!
       advisory_unlock while advisory_locked?
     end
