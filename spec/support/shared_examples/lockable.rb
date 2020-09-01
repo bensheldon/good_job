7fc343e9c1c8210f2171b3b038f4e61bcbe91c0a
Ben Sheldon <bensheldon@gmail.com>
2020-09-01 16:21:28 -0700

    Use `connection.quote_table_name` and add spacing for SQL concatenation

--- spec/support/shared_examples/lockable.rb
+++ spec/support/shared_examples/lockable.rb
@@ -28,7 +28,7 @@ RSpec.shared_examples 'lockable' do
           )
           SELECT "rows"."id"
           FROM "rows"
-          WHERE pg_try_advisory_lock(('x'||substr(md5('good_jobs' || "rows"."id"::text), 1, 16))::bit(64)::bigint)
+          WHERE pg_try_advisory_lock(('x' || substr(md5('good_jobs' || "rows"."id"::text), 1, 16))::bit(64)::bigint)
           LIMIT 2
         )
         ORDER BY "good_jobs"."priority" DESC
