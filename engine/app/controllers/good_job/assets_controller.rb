660c9f3f004569dcc6f8671cc3ecdbf9e0946079
Ben Sheldon [he/him] <bensheldon@gmail.com>
2021-05-24 09:35:02 -0700

    Serve Dashboard assets as discrete paths instead of inlining (#262)
    - Small refactor to seed's variable names

---
+++ engine/app/controllers/good_job/assets_controller.rb
@@ -0,0 +1,29 @@
+module GoodJob
+  class AssetsController < ActionController::Base # rubocop:disable Rails/ApplicationController
+    skip_before_action :verify_authenticity_token
+
+    before_action do
+      expires_in 1.year, public: true
+    end
+
+    def bootstrap_css
+      render file: GoodJob::Engine.root.join("app", "assets", "vendor", "bootstrap", "bootstrap.min.css")
+    end
+
+    def bootstrap_js
+      render file: GoodJob::Engine.root.join("app", "assets", "vendor", "bootstrap", "bootstrap.bundle.min.js")
+    end
+
+    def chartist_css
+      render file: GoodJob::Engine.root.join("app", "assets", "vendor", "chartist", "chartist.css")
+    end
+
+    def chartist_js
+      render file: GoodJob::Engine.root.join("app", "assets", "vendor", "chartist", "chartist.js")
+    end
+
+    def style_css
+      render file: GoodJob::Engine.root.join("app", "assets", "style.css")
+    end
+  end
+end
--- engine/config/routes.rb
+++ engine/config/routes.rb
@@ -1,4 +1,12 @@
 GoodJob::Engine.routes.draw do
   root to: 'dashboards#index'
   resources :active_jobs, only: :show
+
+  scope controller: :assets do
+    get :bootstrap_css
+    get :bootstrap_js
+    get :chartist_css
+    get :chartist_js
+    get :style_css
+  end
 end
