443b4645d0056f3e8b7ae4accda0a5a1ea19f760
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2021-06-01 17:09:18 +0200

    Move deleting jobs to own controller

--- engine/app/controllers/good_job/active_jobs_controller.rb
+++ engine/app/controllers/good_job/active_jobs_controller.rb
@@ -4,11 +4,5 @@ module GoodJob
       @jobs = GoodJob::Job.where("serialized_params ->> 'job_id' = ?", params[:id])
                           .order(Arel.sql("COALESCE(scheduled_at, created_at) DESC"))
     end
-
-    def destroy
-      deleted_count = GoodJob::Job.where(id: params[:id]).delete_all
-      message = deleted_count.positive? ? { notice: "Job deleted" } : { alert: "Job not deleted" }
-      redirect_to root_path, **message
-    end
   end
 end
