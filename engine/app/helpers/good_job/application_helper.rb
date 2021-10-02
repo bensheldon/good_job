8a264acafbdeb3aac616994295d039869af3df88
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2021-10-02 02:48:33 +0200

    Display schedule time relative to now. (#394)

--- engine/app/helpers/good_job/application_helper.rb
+++ engine/app/helpers/good_job/application_helper.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 module GoodJob
   module ApplicationHelper
+    def relative_time(timestamp)
+      text = timestamp.future? ? "in #{time_ago_in_words(timestamp)}" : "#{time_ago_in_words(timestamp)} ago"
+      tag.time(text, datetime: timestamp, title: timestamp)
+    end
   end
 end
