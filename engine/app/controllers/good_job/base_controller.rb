8180313073261f37cb99c824ba812449459235a7
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2021-10-07 17:30:11 +0200

    Follow the official guide on how to switch locale
    https://edgeguides.rubyonrails.org/i18n.html#setting-the-locale-from-user-preferences

--- engine/app/controllers/good_job/base_controller.rb
+++ engine/app/controllers/good_job/base_controller.rb
@@ -3,8 +3,12 @@ module GoodJob
   class BaseController < ActionController::Base # rubocop:disable Rails/ApplicationController
     protect_from_forgery with: :exception
 
-    around_action do
-      I18n.with_locale(:en) { yield }
+    around_action :switch_locale
+
+    private
+
+    def switch_locale(&action)
+      I18n.with_locale(:en, &action)
     end
   end
 end
