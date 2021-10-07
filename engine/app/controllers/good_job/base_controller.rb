5ca5ee950b8103160b517d7e6e502a589fa930d3
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2021-10-07 12:55:58 +0200

    Enforce english locale in UI
    Without it the last set locale in the app is used, which now may display relative time text in different language

--- engine/app/controllers/good_job/base_controller.rb
+++ engine/app/controllers/good_job/base_controller.rb
@@ -2,5 +2,7 @@
 module GoodJob
   class BaseController < ActionController::Base # rubocop:disable Rails/ApplicationController
     protect_from_forgery with: :exception
+
+    before_action -> { I18n.locale = :en }
   end
 end
