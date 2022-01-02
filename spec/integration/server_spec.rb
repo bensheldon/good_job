4198def0305e55fae3a87adc77bd50e76a3cbe37
Ben Sheldon [he/him] <bensheldon@gmail.com>
2022-01-02 10:30:22 -0800

    Clean up server integration tests (#474)

--- spec/integration/server_spec.rb
+++ spec/integration/server_spec.rb
@@ -3,22 +3,25 @@ require 'rails_helper'
 
 RSpec.describe 'Server modes', skip_if_java: true do
   context 'when development async' do
-    it 'successfully starts the server' do
+    it 'successfully runs in the server' do
       env = {
         "RAILS_ENV" => "development",
         "GOOD_JOB_EXECUTION_MODE" => "async",
+        "GOOD_JOB_ENABLE_CRON" => "true",
       }
 
       ShellOut.command('bundle exec rails s', env: env) do |shell|
         wait_until(max: 30) do
           expect(shell.output).to include(/Listening on/)
+          # In development, GoodJob starts up before Puma redirects logs to stdout
+          expect(shell.output).to include(/Enqueued ExampleJob/)
         end
       end
     end
   end
 
   context 'when production async' do
-    it 'successfully runs' do
+    it 'successfully runs in the server' do
       env = {
         "RAILS_ENV" => "production",
         "GOOD_JOB_EXECUTION_MODE" => "async",
@@ -26,30 +29,18 @@ RSpec.describe 'Server modes', skip_if_java: true do
       }
       ShellOut.command('bundle exec rails s', env: env) do |shell|
         wait_until(max: 30) do
+          expect(shell.output).to include(/Listening on/)
           expect(shell.output).to include(/GoodJob started scheduler/)
           expect(shell.output).to include(/GoodJob started cron/)
         end
       end
     end
-  end
-
-  context 'when production async_server' do
-    let(:env) do
-      {
-        "RAILS_ENV" => "production",
-        "GOOD_JOB_EXECUTION_MODE" => "async_server",
-      }
-    end
-
-    it 'starts GoodJob when running web server' do
-      ShellOut.command('bundle exec rails s', env: env) do |shell|
-        wait_until(max: 30) do
-          expect(shell.output).to include(/GoodJob started scheduler/)
-        end
-      end
-    end
 
     it 'does not start GoodJob when running other commands' do
+      env = {
+        "RAILS_ENV" => "production",
+        "GOOD_JOB_EXECUTION_MODE" => "async",
+      }
       ShellOut.command('bundle exec rails db:version', env: env) do |shell|
         wait_until(max: 30) do
           expect(shell.output).to include(/Current version/)
