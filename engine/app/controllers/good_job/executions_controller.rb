eb18770a2dc8336c9a60f97aaf96efaff2634fbb
Brandon Keepers <brandon@opensoul.org>
2022-04-21 10:43:54 -0500

    Remove executions from the dashboard (#576)
    * Remove executions from the dashboard
    
    * Preserve params when redirecting to jobs tab

--- engine/app/controllers/good_job/executions_controller.rb
+++ engine/app/controllers/good_job/executions_controller.rb
@@ -1,14 +1,10 @@
 # frozen_string_literal: true
 module GoodJob
   class ExecutionsController < GoodJob::ApplicationController
-    def index
-      @filter = ExecutionsFilter.new(params)
-    end
-
     def destroy
       deleted_count = GoodJob::Execution.where(id: params[:id]).delete_all
       message = deleted_count.positive? ? { notice: "Job execution deleted" } : { alert: "Job execution not deleted" }
-      redirect_back fallback_location: root_path, **message
+      redirect_back fallback_location: jobs_path, **message
     end
   end
 end
