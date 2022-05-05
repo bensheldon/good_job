dcddfbcfcbb5834c38e5e09349e04349dd2f9dad
Ben Sheldon [he/him] <bensheldon@gmail.com>
2022-05-05 07:25:55 -0700

    Improve development instructions and tooling (rename bin/rails, add bin/appraisal)

--- spec/generators/good_job/install_generator_spec.rb
+++ spec/generators/good_job/install_generator_spec.rb
@@ -6,7 +6,7 @@ describe GoodJob::InstallGenerator, type: :generator, skip_if_java: true do
   around do |example|
     quiet { setup_example_app }
     example.run
-    remove_example_app
+    teardown_example_app
   end
 
   it 'creates a migration for good_jobs table' do
