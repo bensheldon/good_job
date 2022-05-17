27277f2df4a4e86f6ae99074aed2dcbe4939b660
Ben Sheldon [he/him] <bensheldon@gmail.com>
2022-05-17 11:24:40 -0700

    Move latency methods to Execution and rename

--- lib/good_job/execution.rb
+++ lib/good_job/execution.rb
@@ -317,6 +317,20 @@ module GoodJob
       finished_at || performed_at || scheduled_at || created_at
     end
 
+    # Time between when this job was expected to run and when it started running
+    def queue_latency
+      now = Time.zone.now
+      expected_start = scheduled_at || created_at
+      actual_start = performed_at || now
+
+      actual_start - expected_start unless expected_start >= now
+    end
+
+    # Time between when this job started and finished
+    def runtime_latency
+      (finished_at || Time.zone.now) - performed_at if performed_at
+    end
+
     private
 
     def active_job_data
