e07ac8415f42f0e4fa1cd988862dce013ccf2d2e
Ben Sheldon <bensheldon@gmail.com>
2022-06-23 15:11:06 -0700

    Better isolate test environment: run server integration tests on port 3009 with custom pidfile; scope advisory lock counts to test database

--- spec/integration/server_spec.rb
+++ spec/integration/server_spec.rb
@@ -2,6 +2,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Server modes', skip_if_java: true do
+  let(:port) { 3009 }
+  let(:pidfile) { Rails.root.join('tmp/pids/test_server.pid') }
+
   context 'when development async' do
     it 'successfully runs in the server' do
       env = {
@@ -10,7 +13,7 @@ RSpec.describe 'Server modes', skip_if_java: true do
         "GOOD_JOB_ENABLE_CRON" => "true",
       }
 
-      ShellOut.command('bundle exec rails s', env: env) do |shell|
+      ShellOut.command("bundle exec rails s -p #{port} -P #{pidfile}", env: env) do |shell|
         wait_until(max: 30) do
           expect(shell.output).to include(/Listening on/)
           # In development, GoodJob starts up before Puma redirects logs to stdout
@@ -27,7 +30,7 @@ RSpec.describe 'Server modes', skip_if_java: true do
         "GOOD_JOB_EXECUTION_MODE" => "async",
         "GOOD_JOB_ENABLE_CRON" => "true",
       }
-      ShellOut.command('bundle exec rails s', env: env) do |shell|
+      ShellOut.command("bundle exec rails s -p #{port} -P #{pidfile}", env: env) do |shell|
         wait_until(max: 30) do
           expect(shell.output).to include(/Listening on/)
           expect(shell.output).to include(/GoodJob started scheduler/)
--- spec/support/system.rb
+++ spec/support/system.rb
@@ -40,6 +40,7 @@ RSpec.configure do |config|
   config.before(:each, type: :system, js: true) do
     # Chrome's no-sandbox option is required for running in Docker
     driven_by :selenium, using: (ENV['SHOW_BROWSER'] ? :chrome : :headless_chrome), screen_size: [1024, 800] do |driver_options|
+      driver_options.add_option('w3c', false)
       driver_options.add_argument("--disable-dev-shm-usage")
       driver_options.add_argument("--no-sandbox")
     end
