d67d677a8ec6629d1d1a854150a0873e780e15c3
Brandon Keepers <brandon@opensoul.org>
2022-07-26 10:51:31 -0400

    Show basename of proctitle (#679)
    * Show basename of proctitle
    
    * Show full proctitle in title attribute
    
    * Fix lint error in spec

--- spec/lib/models/good_job/process_spec.rb
+++ spec/lib/models/good_job/process_spec.rb
@@ -46,4 +46,18 @@ RSpec.describe GoodJob::Process do
       expect { process.deregister }.to change(described_class, :count).by(-1)
     end
   end
+
+  describe '#basename' do
+    let(:process) { described_class.new state: {} }
+
+    it 'splits proctitle on dir and program name' do
+      process.state['proctitle'] = '/app/bin/good_job'
+      expect(process.basename).to eq('good_job')
+    end
+
+    it 'preserves program arguments' do
+      process.state['proctitle'] = '/Users/me/projects/good_job/bin/bundle exec rails start'
+      expect(process.basename).to eq('bundle exec rails start')
+    end
+  end
 end
