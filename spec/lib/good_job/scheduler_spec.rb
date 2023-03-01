95714e8512073aeb8a760073e9225073b8f2c751
Ben Sheldon [he/him] <bensheldon@gmail.com>
2023-02-28 20:47:08 -0800

    Rewrite flaky tests

--- spec/lib/good_job/scheduler_spec.rb
+++ spec/lib/good_job/scheduler_spec.rb
@@ -144,13 +144,11 @@ RSpec.describe GoodJob::Scheduler do
     it 'uses state[:count] to create multiple threads' do
       job_performer = instance_double(GoodJob::JobPerformer, next: nil, next?: true, name: '', next_at: [], cleanup: nil)
       scheduler = described_class.new(job_performer, max_threads: 1)
+      allow(scheduler).to receive(:create_task)
 
       result = scheduler.create_thread({ count: 10 })
-      if result
-        expect(job_performer).to have_received(:next).exactly(10).times
-      else
-        expect(job_performer).to have_received(:next).at_most(9).times
-      end
+      expect(result).to be true
+      expect(scheduler).to have_received(:create_task).exactly(10).times
     end
   end
 
