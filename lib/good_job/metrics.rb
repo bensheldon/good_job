30db43a7f715d2e84a1569837045376500cbd3aa
Anders Mikkelsen <andersgm@outlook.dk>
2023-06-20 09:43:28 +0200

    added doc

--- lib/good_job/metrics.rb
+++ lib/good_job/metrics.rb
@@ -1,31 +1,44 @@
 # frozen_string_literal: true
 module GoodJob # :nodoc:
+  # Metrics for the scheduler.
   class Metrics
     def initialize
       @failed_count = Concurrent::AtomicFixnum.new
       @succeeded_count = Concurrent::AtomicFixnum.new
     end
 
-    def failed_count
-      @failed_count.value
-    end
-
-    def succeeded_count
-      @succeeded_count.value
-    end
-
+    # Increments number of failed jobs.
+    # @return [Integer]
     def increment_failed
       @failed_count.increment
     end
 
+    # Increments number of succeeded jobs.
+    # @return [Integer]
     def increment_succeeded
       @succeeded_count.increment
     end
 
+    # Failed job count.
+    # @return [Integer]
+    def failed_count
+      @failed_count.value
+    end
+
+    # Number of succeeded jobs.
+    # @return [Integer]
+    def succeeded_count
+      @succeeded_count.value
+    end
+
+    # Total number of jobs (failed + succeeded).
+    # @return [Integer]
     def total_count
       failed_count + succeeded_count
     end
 
+    # Reset counters.
+    # @return [void]
     def reset
       @failed_count.value = 0
       @succeeded_count.value = 0
