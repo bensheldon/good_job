d89accb62b8fa4d072067050aa78c33dad61011d
Ben Sheldon [he/him] <bensheldon@github.com>
2023-12-11 06:37:49 -0800

    Update lib/good_job/systemd_service.rb

--- lib/good_job/systemd_service.rb
+++ lib/good_job/systemd_service.rb
@@ -11,7 +11,7 @@ module GoodJob # :nodoc:
   #
   class SystemdService
     def self.task_observer(_time, _output, thread_error) # :nodoc:
-      return if thread_error.is_a?(Concurrent::CancelledOperationError) || thread_error.blank?
+      return if !thread_error || thread_error.is_a?(Concurrent::CancelledOperationError)
 
       ActiveSupport::Notifications.instrument("systemd_watchdog_error.good_job", { error: thread_error })
       GoodJob._on_thread_error(thread_error)
