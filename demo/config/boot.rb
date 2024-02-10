335be451a5e05e193344d25a4d4ede5dc1dedcc1
Ben Sheldon [he/him] <bensheldon@gmail.com>
2024-02-10 08:44:53 -0800

    Use Ruby 3.3 for development; add Bootsnap

--- demo/config/boot.rb
+++ demo/config/boot.rb
@@ -2,4 +2,11 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __dir__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+
+begin
+  require 'bootsnap/setup'
+rescue LoadError
+  nil
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __dir__)
