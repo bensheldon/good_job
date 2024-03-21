d3ae492d6ad200ebe4dc7ff0ab83f2361afc24b0
mec rawlings <mec.rawlings@gmail.com>
2024-03-21 16:18:52 +0000

    Add spec to cover PgHero basic auth
    We want to be sure PgHero is behind basic auth. This spec validates that
    once the correct environment variables are set, PgHero will require
    basic authentication.
    
    We have to use the rack_test driver so we can use `basic_authorize` to
    set the headers, rack_test is the default in RSpec/Capybara, but we have
    explicitly set the driver just in case.

---
+++ spec/system/pghero_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'PGHero', driver: :rack_test do
+  describe 'access' do
+    context 'with the appropriate environment variables set' do
+      before do
+        allow(ENV).to receive(:[]).with('PGHERO_USERNAME').and_return('link')
+        allow(ENV).to receive(:[]).with('PGHERO_PASSWORD').and_return('hyrule')
+      end
+
+      it 'fails without the basic auth credentials' do
+        visit pg_hero_path
+
+        expect(status_code).to eq 401
+        expect(page).to have_content 'HTTP Basic: Access denied.'
+      end
+
+      it 'succeeds with the credentials' do
+        # this will only work with the rack_test driver
+        page.driver.browser.basic_authorize 'link', 'hyrule'
+
+        visit pg_hero_path
+
+        expect(status_code).to eq 200
+        expect(page).to have_content 'PgHero'
+      end
+    end
+  end
+end
