ced3f7aec073869224261664201b4f58e5b7547d
Arnaud Levy <contact@arnaudlevy.com>
2024-05-27 07:02:28 +0200

    linebreaks

--- app/controllers/good_job/statistics_controller.rb
+++ app/controllers/good_job/statistics_controller.rb
@@ -2,18 +2,21 @@
 
 module GoodJob
   class StatisticsController < ApplicationController
+
     def index
       @job_classes = GoodJob::Execution.pluck(:job_class).uniq.sort
     end
 
     def show
-      @job_class = params[:id]
+      @job_class  = params[:id]
       @chart_data = StatisticsJobClassChart.new(@job_class).data
-      @count = GoodJob::Execution.where(job_class: @job_class).finished.count
-      @runtimes = GoodJob::Execution.where(job_class: @job_class)
-                                    .finished.map { |execution| execution.runtime_latency }
-                                    .compact
-
+      @count      = GoodJob::Execution.where(job_class: @job_class)
+                                      .finished
+                                      .count
+      @runtimes   = GoodJob::Execution.where(job_class: @job_class)
+                                      .finished
+                                      .map { |execution| execution.runtime_latency }
+                                      .compact
     end
   end
 end
