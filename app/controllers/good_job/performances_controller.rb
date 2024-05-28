9d35a05f034d5ba215ecef8a2aa9f335cde214fa
Arnaud Levy <contact@arnaudlevy.com>
2024-05-28 17:34:27 +0200

    performance

--- app/controllers/good_job/performances_controller.rb
+++ app/controllers/good_job/performances_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module GoodJob
-  class StatisticsController < ApplicationController
+  class PerformancesController < ApplicationController
 
     def index
       @job_classes = GoodJob::Execution.pluck(:job_class).uniq.sort
