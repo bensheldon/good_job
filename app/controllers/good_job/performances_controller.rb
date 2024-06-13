a37f3fac4e08d7503c8fd98718b7855471cd2699
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-06-13 18:47:25 +0200

    clean performances controller

--- app/controllers/good_job/performances_controller.rb
+++ app/controllers/good_job/performances_controller.rb
@@ -2,15 +2,8 @@
 
 module GoodJob
   class PerformancesController < ApplicationController
-
     def index
       @job_classes = GoodJob::Execution.pluck(:job_class).uniq.sort
-    end
-
-    protected
-
-    def executions
-      GoodJob::Execution.where(job_class: @job_class).finished
     end
   end
 end
