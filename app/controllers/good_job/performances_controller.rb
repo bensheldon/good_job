f7391eed85757b8e0ee05df33cd5c62bbff4b72e
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-06-14 10:31:50 +0200

    performance tab: single sql request

--- app/controllers/good_job/performances_controller.rb
+++ app/controllers/good_job/performances_controller.rb
@@ -3,7 +3,17 @@
 module GoodJob
   class PerformancesController < ApplicationController
     def index
-      @job_classes = GoodJob::Execution.pluck(:job_class).uniq.sort
+      @performances = GoodJob::DiscreteExecution
+                        .where.not(job_class: nil)
+                        .group(:job_class)
+                        .select("
+                          job_class,
+                          COUNT(*) AS executions_count,
+                          AVG(EXTRACT(EPOCH FROM (finished_at - created_at))) AS avg_duration,
+                          MIN(EXTRACT(EPOCH FROM (finished_at - created_at))) AS min_duration,
+                          MAX(EXTRACT(EPOCH FROM (finished_at - created_at))) AS max_duration
+                        ")
+                        .order("job_class")
     end
   end
 end
