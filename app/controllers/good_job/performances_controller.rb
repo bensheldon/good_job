f3f384e4373df55809b7c03aa9293451d240a9ed
Ben Sheldon [he/him] <bensheldon@gmail.com>
2024-07-05 16:19:35 -0700

    Tweak html and css and add a smoketest

--- app/controllers/good_job/performances_controller.rb
+++ app/controllers/good_job/performances_controller.rb
@@ -3,17 +3,21 @@
 module GoodJob
   class PerformancesController < ApplicationController
     def index
-      @performances = GoodJob::DiscreteExecution
+      if GoodJob::DiscreteExecution.monotonic_duration_migrated?
+        @performances = GoodJob::DiscreteExecution
                         .where.not(job_class: nil)
                         .group(:job_class)
                         .select("
-                          job_class,
-                          COUNT(*) AS executions_count,
-                          AVG(duration) AS avg_duration,
-                          MIN(duration) AS min_duration,
-                          MAX(duration) AS max_duration
-                        ")
+                            job_class,
+                            COUNT(*) AS executions_count,
+                            AVG(duration) AS avg_duration,
+                            MIN(duration) AS min_duration,
+                            MAX(duration) AS max_duration
+                          ")
                         .order("job_class")
+      else
+        @needs_upgrade = true
+      end
     end
   end
 end
---
+++ spec/system/performance_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Performance Page', :js do
+  before do
+    ActiveJob::Base.queue_adapter = GoodJob::Adapter.new(execution_mode: :external)
+    ExampleJob.perform_later
+    GoodJob.perform_inline
+  end
+
+  it 'renders properly' do
+    visit good_job.root_path
+    click_link 'Performance'
+    expect(page).to have_selector 'h2', text: 'Performance'
+
+    expect(page).to have_content 'ExampleJob'
+  end
+end
