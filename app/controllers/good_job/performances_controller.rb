400061bcd09ae61b7eaa6107e208994d3759c23e
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-07-05 18:05:50 +0200

    use duration instead of extracting epoch

--- app/controllers/good_job/performances_controller.rb
+++ app/controllers/good_job/performances_controller.rb
@@ -9,9 +9,9 @@ module GoodJob
                         .select("
                           job_class,
                           COUNT(*) AS executions_count,
-                          AVG(EXTRACT(EPOCH FROM (finished_at - created_at))) AS avg_duration,
-                          MIN(EXTRACT(EPOCH FROM (finished_at - created_at))) AS min_duration,
-                          MAX(EXTRACT(EPOCH FROM (finished_at - created_at))) AS max_duration
+                          AVG(duration) AS avg_duration,
+                          MIN(duration) AS min_duration,
+                          MAX(duration) AS max_duration
                         ")
                         .order("job_class")
     end
