9a1eb0b83b2bc8768e666eb4080039e9c90709ff
Ben Sheldon [he/him] <bensheldon@gmail.com>
2024-07-08 11:04:13 -0700

    Actually remove deprecated configuration for `cleanup_interval_seconds`, `cleanup_interval_jobs`; remove deprecated Lockable` (#1406)

--- lib/good_job/cleanup_tracker.rb
+++ lib/good_job/cleanup_tracker.rb
@@ -9,7 +9,7 @@ module GoodJob # :nodoc:
                   :last_at
 
     def initialize(cleanup_interval_seconds: false, cleanup_interval_jobs: false)
-      raise ArgumentError, "Do not use `0`. Use `false` to disable, or -1 to always run" if cleanup_interval_seconds == 0 || cleanup_interval_jobs == 0 # rubocop:disable Style/NumericPredicate
+      raise ArgumentError, "Do not use `0` for cleanup intervals. Use `false` to disable, or -1 to always run" if cleanup_interval_seconds == 0 || cleanup_interval_jobs == 0 # rubocop:disable Style/NumericPredicate
 
       self.cleanup_interval_seconds = cleanup_interval_seconds
       self.cleanup_interval_jobs = cleanup_interval_jobs
