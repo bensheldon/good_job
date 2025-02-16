3b4fb8419a1e09b2caf447fbe18fe63ea2f068d0
Ben Sheldon [he/him] <bensheldon@github.com>
2025-02-16 09:28:19 -0800

    Update 20250216102816_add_index_good_jobs_concurrency_key_created_at.rb

--- demo/db/migrate/20250216102816_add_index_good_jobs_concurrency_key_created_at.rb
+++ demo/db/migrate/20250216102816_add_index_good_jobs_concurrency_key_created_at.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddIndexGoodJobsConcurrencyKeyCreatedAt <ActiveRecord::Migration[7.1]
+class AddIndexGoodJobsConcurrencyKeyCreatedAt < ActiveRecord::Migration[7.1]
   def change
     add_index :good_jobs, [:concurrency_key, :created_at]
   end
