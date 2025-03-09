383a87f7085e6b8764fb1cb18edcbf66fdb1d1a5
Adrien S <adrien@siami.fr>
2025-03-09 16:21:29 +0100

    Add index on good_jobs: [:concurrency_key, :created_at] to improve performance of throttling (#1603) (#1605)
    * Add index on good_jobs: [:concurrency_key, :cerated_at] to improve performance of throttling (#1603)
    
    * Update 20250216102816_add_index_good_jobs_concurrency_key_created_at.rb
    
    * Update 20250216102816_add_index_good_jobs_concurrency_key_created_at.rb
    
    * Update 03_add_index_good_jobs_concurrency_key_created_at.rb.erb
    
    * Add the index concurrently
    
    * restore migration version 7.1
    
    * Ensure migration is idempotent
    
    ---------
    
    Co-authored-by: Ben Sheldon [he/him] <bensheldon@github.com>
    Co-authored-by: Ben Sheldon [he/him] <bensheldon@gmail.com>

--- demo/db/schema.rb
+++ demo/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_08_01_143343) do
+ActiveRecord::Schema.define(version: 2025_02_17_090233) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "pgcrypto"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 2024_08_01_143343) do
     t.index ["active_job_id", "created_at"], name: "index_good_jobs_on_active_job_id_and_created_at"
     t.index ["batch_callback_id"], name: "index_good_jobs_on_batch_callback_id", where: "(batch_callback_id IS NOT NULL)"
     t.index ["batch_id"], name: "index_good_jobs_on_batch_id", where: "(batch_id IS NOT NULL)"
+    t.index ["concurrency_key", "created_at"], name: "index_good_jobs_on_concurrency_key_and_created_at"
     t.index ["concurrency_key"], name: "index_good_jobs_on_concurrency_key_when_unfinished", where: "(finished_at IS NULL)"
     t.index ["cron_key", "created_at"], name: "index_good_jobs_on_cron_key_and_created_at_cond", where: "(cron_key IS NOT NULL)"
     t.index ["cron_key", "cron_at"], name: "index_good_jobs_on_cron_key_and_cron_at_cond", unique: true, where: "(cron_key IS NOT NULL)"
