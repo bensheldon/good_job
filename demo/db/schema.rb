10876e85768e88fdfb660c26dada3887a18db592
Michael Fowler <michael@delivereasy.co.nz>
2025-09-19 11:51:16 +1200

    Create a partial index on `good_jobs.finished_at` where those values are NOT NULL
    This will better support the implementation of `cleanup_preserved_jobs`.  The
    existing partial index here also requires that `retried_good_job_id` be NULL,
    but that column was effectively written out in https://github.com/bensheldon/good_job/pull/1430

--- demo/db/schema.rb
+++ demo/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_02_17_090233) do
+ActiveRecord::Schema.define(version: 2025_09_18_235018) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "pgcrypto"
