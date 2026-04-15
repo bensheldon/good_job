5f50bec82f6f9fcce2b2fa1c3434c5ef25c6e975
Ben Sheldon [he/him] <bensheldon@gmail.com>
2026-04-14 21:16:38 -0700

    Fix schema.rb: use plpgsql without pg_catalog namespace

--- demo/db/schema.rb
+++ demo/db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2026_04_14_000000) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
+  enable_extension "plpgsql"
   enable_extension "pg_stat_statements"
   enable_extension "pgcrypto"
   enable_extension "uuid-ossp"
