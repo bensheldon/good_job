47049476b82aaa278ef5045e19d1fbd5a4f89c8f
Ben Sheldon [he/him] <bensheldon@gmail.com>
2026-04-17 08:22:16 -0700

    InterruptedError inherits from InterruptError

--- lib/good_job/interrupted_error.rb
+++ lib/good_job/interrupted_error.rb
@@ -4,6 +4,6 @@ module GoodJob
   # Represents a previous execution that was interrupted by a SIGKILL or power failure.
   # Used only to format the error string stored in the database for the interrupted execution record.
   # This error is never raised and cannot be rescued; use +GoodJob::InterruptError+ for that.
-  class InterruptedError < StandardError
+  class InterruptedError < InterruptError
   end
 end
